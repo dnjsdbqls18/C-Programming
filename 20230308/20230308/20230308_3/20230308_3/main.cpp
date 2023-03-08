e58bc27fa1baffe90006168287b32a561bb7c449
dnjsdbqls18 <126652295+dnjsdbqls18@users.noreply.github.com>
2023-03-08 10:13:14 +0900

    Add files via upload

---
+++ 20230308/20230308/20230308_3/20230308_3/main.cpp
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(void)
+{
+
+	printf("1 x 1 = 1\n");
+	printf("1 x 2 = 2\n");  // \n line feed
+	printf("1 x 3 = 3\n");  // \n line feed
+	printf("1 x 4 = 4\n");  // \n line feed
+	printf("1 x 5 = 5\n");  // \n line feed
+	printf("1 x 6 = 6\n");  // \n line feed
+	printf("1 x 7 = 7\n");  // \n line feed
+	printf("1 x 8 = 8\n");  // \n line feed
+	printf("1 x 9 = 9 \n");  // \n line feed
+
+
+	return 0;
+}
