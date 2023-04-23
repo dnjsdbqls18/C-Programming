7a00979d9d56ece4474656b5114467ba3d039d0c
dnjsdbqls18 <126652295+dnjsdbqls18@users.noreply.github.com>
2023-04-23 21:40:00 +0900

    Add files via upload

---
+++ 20230423_21~25/20230423_25/20230423_25/main.cpp
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main(void)
+{
+	int x, y;
+
+	for (y = 1; y < 10000; y++) // ÁßÃ¸ ·çÇÁ
+	{
+		for (x = 1; x < 10; x++)
+		{
+			if ( _kbhit())
+				goto OUT;
+			printf("*");
+		}
+		printf("\n");
+	}
+OUT:
+	return 0;
+}
