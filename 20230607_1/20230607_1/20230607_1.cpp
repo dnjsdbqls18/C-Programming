5a5b3b0b77074a69c50927e53afe98663b57d0c8
dnjsdbqls18 <126652295+dnjsdbqls18@users.noreply.github.com>
2023-07-19 21:21:42 +0900

    Add files via upload

---
+++ 20230607_1/20230607_1/20230607_1.cpp
@@ -0,0 +1,30 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define SIZE 100
+
+int main(void)
+{
+	srand(time(NULL));
+
+	int freq[SIZE] = { 0 };
+	int i;
+
+	for (i = 0; i < 30000; i++)
+	{
+		++freq[rand() % 100];
+
+	}
+
+	printf("=======================\n");
+	printf("¼ýÀÚ   ºóµµ\n");
+	printf("=======================\n");
+
+	for (i = 0; i < SIZE; i++)
+	{
+		printf("%3d    %3d\n", i, freq[i]);
+	}
+	return 0;
+}
