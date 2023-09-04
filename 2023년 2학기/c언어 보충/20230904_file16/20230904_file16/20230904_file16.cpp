55bc93b7090a138f8616e769b65a6ea570c2b819
dnjsdbqls18 <126652295+dnjsdbqls18@users.noreply.github.com>
2023-09-04 18:47:01 +0900

    Add files via upload

---
+++ 2023년 2학기/c언어 보충/20230904_file16/20230904_file16/20230904_file16.cpp
@@ -0,0 +1,27 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main(void) {
+
+	FILE* fp;
+	int cnt = 0;
+
+	fopen_s(&fp, "text.txt", "wt");
+	fputc('a', fp);
+	fputc('b', fp);
+
+	fclose(fp);
+
+	fopen_s(&fp, "text.txt", "rt");
+	while (!feof(fp)) {
+		cnt++;
+		printf("%d ", feof(fp));
+		printf("%c ", fgetc(fp));
+		printf("%d \n", feof(fp));
+	}
+	printf("\ncnt = %d\n", cnt);
+
+	fclose(fp);
+
+	return 0;
+}
