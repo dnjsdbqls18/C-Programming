9e3a6b3e1a706aafc18d52da81c659661694f425
dnjsdbqls18 <126652295+dnjsdbqls18@users.noreply.github.com>
2023-09-15 17:45:36 +0900

    Add files via upload

---
+++ 2023년 2학기/c언어 보충/20230913/20230913_2/20230913_2/20230913_2.cpp
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+void ChangePlusOne(int n)
+{
+	n += 1;
+}
+
+int main(void)
+{
+	int number = 3;
+
+	printf("%d\n", number);
+	number = 5;
+	printf("%d\n", number);
+	ChangePlusOne(number);
+	printf("%d\n", number);
+
+	return 0;
+}
