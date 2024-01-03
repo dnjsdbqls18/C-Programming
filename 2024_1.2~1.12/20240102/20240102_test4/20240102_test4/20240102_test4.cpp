d1f596cdf41da4f0d80f82602837485f5a7a5425
dnjsdbqls18 <126652295+dnjsdbqls18@users.noreply.github.com>
2024-01-03 14:30:21 +0900

    Add files via upload

---
+++ 2024_1.2~1.12/20240102/20240102_test4/20240102_test4/20240102_test4.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+	int test[10][10];
+
+	for (int i = 0; i < 10; i++)
+	{
+		for (int j = 0; j < 10; j++)
+		{
+			test[i][0]
+		}
+	}
+}
+
+//대각선 숫자
+// 0으로 초기화하고 다이아몬드 모형으로 프로그래밍
