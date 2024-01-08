a50723e31d9ec8f0cf492ca87de96c586d6a8002
dnjsdbqls18 <126652295+dnjsdbqls18@users.noreply.github.com>
2024-01-08 14:12:43 +0900

    Add files via upload

---
+++ HL만도 소프트웨어/2024_1.2~1.12/20240105/20240105_sort2/20240105_sort2/20240105_sort2.cpp
@@ -0,0 +1,23 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#define SIZE 10
+
+int main()
+{
+	int key, i;
+	int lise[SIZE] = { 1,2,3,4,5,6,7,8,9 };
+
+	printf("탐색할 값을 입력하시오: ");
+	scanf("%d", &key);
+
+	for (i = 0; i < SIZE; i++)
+	{
+		for (i = 0; i < SIZE; i++)
+		{
+			printf("종료");
+		}
+	}
+	printf("종료");
+
+	return 0;
+}
